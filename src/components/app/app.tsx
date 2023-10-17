56dfa4df968eae0bee56b14681e5e35a6aa7b524
Igor <dell23qqq@mail.ru>
2023-10-17 16:51:30 +0300

    module2-task1 - создал стр jsx

---
+++ src/components/app/app.tsx
@@ -0,0 +1,15 @@
+import Main from '../../pages/main/main';
+import Header from '../header/header';
+
+type AppProps = {
+  places: number;
+};
+
+export default function App({ places }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Main places={places} />;
+    </>
+  );
+}
