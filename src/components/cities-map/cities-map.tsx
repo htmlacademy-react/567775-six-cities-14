46db8676987cf8127ef883986d4ca4e3ec497745
Sinedd23 <dell23qqq@mail.ru>
2023-11-06 14:20:19 +0300

    module5-task1: подключил компонент с картой leaflet

--- src/components/cities-map/cities-map.tsx
+++ src/components/cities-map/cities-map.tsx
@@ -2,6 +2,6 @@ import { mapDataCity, mapDataPoints } from '../../mocks/map';
 import { TMap } from '../../types/map';
 import { Map } from '../map';
 
-export const CitiesMap: React.FC<TMap> = () => {
-  return <Map city={mapDataCity} points={mapDataPoints} selectedPoint={undefined} />;
-};
+export const CitiesMap: React.FC<TMap> = () => (
+  <Map city={mapDataCity} points={mapDataPoints} selectedPoint={undefined} />
+);
