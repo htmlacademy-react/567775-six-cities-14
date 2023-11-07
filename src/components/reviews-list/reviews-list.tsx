0cea72ed546ca69d7744aae84e2b4fb6540a1fc9
Igor <dell23qqq@mail.ru>
2023-11-07 16:56:49 +0300

    module5-task2: reviews новые блоки

--- src/components/reviews-list/reviews-list.tsx
+++ src/components/reviews-list/reviews-list.tsx
@@ -1,17 +1,15 @@
 import { TReviewsList } from '../../types/reviews';
 import { ReviewsItem } from '../reviews-item';
 
-export const ReviewsList: React.FC<TReviewsList> = ({ list }: TReviewsList) => {
-  return (
-    <>
-      <h2 className="reviews__title">
-        Reviews · <span className="reviews__amount">{list.length}</span>
-      </h2>
-      <ul className="reviews__list">
-        {list.map((props) => (
-          <ReviewsItem {...props} key={props.id} />
-        ))}
-      </ul>
-    </>
-  );
-};
+export const ReviewsList: React.FC<TReviewsList> = ({ list }: TReviewsList) => (
+  <>
+    <h2 className="reviews__title">
+      Reviews · <span className="reviews__amount">{list.length}</span>
+    </h2>
+    <ul className="reviews__list">
+      {list.map((props) => (
+        <ReviewsItem {...props} key={props.id} />
+      ))}
+    </ul>
+  </>
+);
