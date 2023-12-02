ee8aa8cbd459af1dd6313c945b483d4c40a2c52c
Sinedd23 <dell23qqq@mail.ru>
2023-12-02 12:29:26 +0300

    module8-task1

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,45 +1,4 @@
 import { createAction } from '@reduxjs/toolkit';
-// import { TCityOptions } from '../types/city';
-// import { TSortingOffers } from '../types/sorting';
-// import { TOfferItemProps } from '../types/offers';
 import { AppRouter } from '../../consts';
-// import { ReviewsItemProps } from '../types/reviews';
-
-// export const setCityActive = createAction<{ city: TCityOptions }>(
-//   'setCityActive'
-// );
-// export const setSorting = createAction<{ sorting: TSortingOffers }>(
-//   'setSorting'
-// );
-// export const setOffers = createAction<TOfferItemProps[]>('setOffers');
-// export const getOffers = createAction('getOffers');
-// export const setOffersIsLoading = createAction<boolean>('setOffersIsLoading');
-// export const requireAuthorization = createAction<AuthorizationStatus>(
-//   'requireAuthorization'
-// );
-// export const setError = createAction<string | null>('setError');
-// export const redirectToRoute = createAction<AppRouter>('redirectToRoute');
-// export const setOfferDetail = createAction<TOfferItemProps>('setOfferDetail');
-// export const setOfferDetailIsLoading = createAction<boolean>(
-//   'setOfferDetailIsLoading'
-// );
-// export const setOfferDetailIsNotFound = createAction<boolean>(
-//   'setOfferDetailIsNotFound'
-// );
-// export const setOfferComments =
-//   createAction<ReviewsItemProps[]>('setOfferComments');
-// export const setOfferCommentsIsLoading = createAction<boolean>(
-//   'setOfferCommentsIsLoading'
-// );
-// export const setOfferCommentsIsNotFound = createAction<boolean>(
-//   'setOfferCommentsIsNotFound'
-// );
-// export const setOffersNearby = createAction<TOfferItemProps[]>('setOffersNearby');
-// export const setOffersNearbyIsLoading = createAction<boolean>(
-//   'setOffersNearbyIsLoading'
-// );
-// export const setOffersNearbyIsNotFound = createAction<boolean>(
-//   'setOffersNearbyIsNotFound'
-// );
 
 export const redirectToRoute = createAction<AppRouter>('redirectToRoute');
